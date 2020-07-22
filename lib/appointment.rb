2b27804b38623006f551fdfab5ad6535a8a5854c
Michael Waide <mvwaide@gmail.com>
2020-07-22 15:38:14 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,27 +1,9 @@
 
-class Appointment
+class Appointment	
+  attr_accessor :date, :doctor, :patient	
 
-  attr_reader :patient, :doctor, :date
-
-  @@all = []
-
-  def initialize(patient ="", doctor = "", date = "")
-    @patient = patient
-    @doctor = doctor
-    @date = date
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def patients
-    self.patient
-  end
-
-  def doctors
-    self.doctor
-  end
-
-end
+  def initialize(date, doctor)	
+    @date = date	
+    @doctor = doctor	
+    doctor.add_appointment(self)	
+  end	
