a067624d7ebdad05869bc68dbf853d40b5c8223d
Michael Waide <mvwaide@gmail.com>
2020-07-22 15:45:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,31 +14,3 @@ class Song
     @@all
   end
 end
-
-class Doctor
-  @@all=[]
-
-  def self.all
-    @@all
-  end
-
-  attr_accessor :name, :appointments, :patient
-
-  def initialize(name)
-    @name = name
-    @appointments = []
-    @@all << self
-  end
-
-  def new_appointment(date, patient)
-     appointment = Appointment.new(patient, date, self)
-     @appointments << appointment
-     appointment
-  end
-
-  def patients
-    @appointments.collect do |appointment|
-      appointment.patient
-    end
-  end
-end
