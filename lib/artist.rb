16640ff4be29b58585f6461ef3f20cb3ea3b6d96
Michael Waide <mvwaide@gmail.com>
2020-07-22 15:39:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,21 +1,31 @@
-class Artist
-  attr_accessor :name
+class Artist	
+  attr_accessor :name	
 
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
+  @@all = []	
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
+  def initialize(name)	
+    @name = name	
+    @@all << self	
+  end	
 
-  def songs
-    @songs
-  end
+  def self.all	
+    @@all	
+  end	
 
-  def genres
-    self.songs.collect {|song| song.genre}
-  end
-end 
+  def add_song(song)	
+    song.artist = self	
+  end	
+
+  def add_song_by_name(name)	
+    song = Song.new(name)	
+    song.artist = self	
+  end	
+
+  def songs	
+    Song.all.select {|song| song.artist == self}	
+  end	
+
+  def self.song_count	
+    Song.all.count	
+  end	
+end
