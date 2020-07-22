4a9f2bf93fd36b4152cddddf60d9dfabaa78d809
Michael Waide <mvwaide@gmail.com>
2020-07-22 15:24:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,24 +1,28 @@
 class Artist
 
-attr_accessor :name
+  attr_accessor :name
+  @@all = []
 
-@@all = []
-
-def initialize(name)
-  @name = name
-  @@all << self
-end
-
- def songs
-   Song.all.select {|song| song.artist == self}
- end
-
- def genres
-   songs.map {|song| song.genre}
+  def initialize(name)
+    @name = name
+    @@all << self
   end
 
-def self.all
-  @@all
-end
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def genres
+    songs.map {|song| song.genre}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(song, genre)
+    Song.new(name, self, genre)
+  end
+
 
 end
