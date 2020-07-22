ff4b8ad6e3076b18aa584dfae7c21da1948560d9
Michael Waide <mvwaide@gmail.com>
2020-07-22 15:22:15 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -9,3 +9,18 @@ def initialize(name)
 end
 
 def self.all
+  @all
+end
+
+def appointments
+  Appointment.all.select {|appointment| appointment.patient == self}
+end
+
+def doctors
+  appointments.collect {|appointment| appointment.doctor}
+end
+
+def new_appointment(doctor, date)
+  Appointment.new(self, date, doctor)
+end
+end
