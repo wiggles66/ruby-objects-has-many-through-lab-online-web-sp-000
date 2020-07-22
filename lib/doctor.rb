989a8be0f47fa30ec6db9bc888cb0802a85ce1fd
Michael Waide <mvwaide@gmail.com>
2020-07-22 15:38:30 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,32 +1,21 @@
-class Doctor
+class Doctor	
+  attr_accessor :name	
 
-  attr_reader :name, :appointment, :patient
+  def initialize(name)	
+    @name = name	
+    @appointments = []	
+  end	
 
-  @@all_doc = []
+  def add_appointment(appointment)	
+    @appointments << appointment 	
+    appointment.doctor = self	
+  end	
 
-  def initialize(name)
-    @name = name
-    @@all_doc << self
-  end
+  def appointments	
+    @appointments	
+  end	
 
-  def self.all
-    @@all_doc
-  end
-
-  def new_appointment(patient, date)
-    Appointment.new(patient, self, date)
-  end
-
-  def appointments
-    Appointment.all.select do |appointment|
-      appointment.doctor == self
-    end
-  end
-
-  def patients
-    appointments.map do |appointment|
-      appointment.patients
-    end
-  end
-
-end
+  def patients	
+    self.appointments.collect {|appointment| appointment.patient}	
+  end	
+end 
