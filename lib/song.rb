63fafbdda24a578a231601eb9c6237736a86e55d
Michael Waide <mvwaide@gmail.com>
2020-07-22 15:38:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,26 +1,9 @@
+class Song	
+  attr_accessor :name, :artist, :genre	
 
-
-require 'pry'
-
-class Song
-
-  attr_accessor :name, :artist, :genre
-
-  ALL = []
-
-  def initialize(name="", artist="", genre="")
-    @name = name
-    @artist = artist
-    @genre = genre
-    ALL << self
-  end
-
-  def self.all
-    ALL
-  end
-
-  def artist
-    @artist
-  end
-
-end
+  def initialize(name, genre)	
+    @name = name	
+    @genre = genre	
+    genre.add_song(self)	
+  end	
+end 
